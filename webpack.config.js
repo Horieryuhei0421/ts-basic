b3e02158438e984e393b73735c35acff95e56d3c
Horieryuhei0421 <s1f101903478@iniad.org>
2021-09-14 18:15:06 +0900

    setup

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        loader: 'ts-loader',
+        test: /\.ts$/
+      }
+    ]
+  }
+}
