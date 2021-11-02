cf8dafec34cb855793fd3f85b847daac089051d2
Horieryuhei0421 <s1f101903478@iniad.org>
2021-11-02 11:27:02 +0900

    lint設定調整

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import World from './world'
 const root: HTMLElement | null = document.getElementById('root')
-const world = new World('Hello World!!!')
+const world = new World('Hello 堀江!!!')
 world.sayHello(root)
