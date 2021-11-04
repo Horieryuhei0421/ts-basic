2fa67fd0a1396e05abf53eef51cf8c4d8841e935
Horieryuhei0421 <s1f101903478@iniad.org>
2021-11-04 17:34:23 +0900

    function

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,20 @@
 // world.sayHello(root)
 
 // 「03.基本の型定義」のサンプルコード
-import { primitiveSample, notExistSample, anySample, unknownSample } from './basic'
+// import { primitiveSample, notExistSample, anySample, unknownSample } from './basic'
 // anySample()
 // notExistSample()
 // primitiveSample()
-unknownSample()
+// unknownSample()
+
+// 「04.関数の型定義」のサンプルコード
+import { logMessage, logMessage5 } from './function/basic'
+import { isUserSignedIn, isUserSignedIn2, sumProductsPrice } from './function/parameters'
+
+logMessage('Hello TypeScript!')
+logMessage5('Hello Type Signature!')
+isUserSignedIn('ABC', 'Torahack')
+isUserSignedIn('123')
+isUserSignedIn2('ABC')
+const sum = sumProductsPrice(1000, 2000, 500, 3000, 250)
+console.log('Function parameters sample 5: Total price:', sum)
